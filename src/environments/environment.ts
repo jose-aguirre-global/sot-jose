6296bdda60c984a2f343186ea9a910ab07ac1a24
Jose Aguirre <jaguirre@globalmaster.com.mx>
2021-10-18 11:08:07 -0500

    Leo 2 socket server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   apiUrl: 'https://localhost:44373/',
   //apiUrl          : 'http://hades.gmm.local:8070/',
   fingerprintAPI  : 'http://hades.gmm.local:3100/',
-  socketServer    : 'http://hades.gmm.local:3100/'
+  socketServer    : 'http://calixto.gmm.local:5300/'
   //socketServer    : 'http://10.0.20.145:3100/'
 
 };
